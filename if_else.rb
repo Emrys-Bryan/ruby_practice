1b8773e43992d2d3f34bf5c38c9761b87841b4d9
Emrys-Bryan <bryan.ibarra3522@gmail.com>
2022-05-27 14:47:07 -0400

    Ejemplos de if

---
+++ if_else.rb
@@ -0,0 +1,18 @@
+print "Igresa tu edad:"
+edad = gets.chomp.to_i
+
+if edad <= 18
+    puts 'Eres menor de edad'
+elsif edad >= 63
+    puts 'Eres adulto mayor'
+else
+    puts 'Eres adulto'
+end
+#Comparacion de string 
+print "Ingresa el nombre de una fruta: "
+fruta= gets.chomp
+if fruta == 'banana'
+    puts "Que rica fruta es la #{fruta.upcase} hay que hacer un licuado"
+else
+    puts "Esa fruta no me gusta"
+end
