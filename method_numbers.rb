13b10934ff7eafde8b6494a0f232aae4e200b5a0
Emrys-Bryan <bryan.ibarra3522@gmail.com>
2022-05-27 10:18:28 -0400

    agregando metodo para numeros

---
+++ method_numbers.rb
@@ -0,0 +1,20 @@
+numero_entero = 1776
+#metodo para números enteros siguiente o sucesor 
+puts numero_entero.next
+puts  numero_siguiente = numero_entero.succ
+#metodo para validar si un número es par o impar
+puts numero_entero.odd?
+puts numero_entero.even?
+puts numero_siguiente.odd?
+puts numero_siguiente.even?
+#conversion de entero a flotante
+puts numero_siguiente/numero_entero.to_f
+puts 800/270.to_f
+#Longitud de un número separar numero por digitos
+print 356.digits
+print 78905.digits
+#redondear un número flotante
+puts 5609.78203.round 
+puts 5609.78203.to_i
+puts 5609.78203.floor 
+puts 5609.78203.ceil
---
+++ variables.rb
@@ -0,0 +1,10 @@
+x = 10
+y = 20
+suma = x + y
+puts suma
+
+precio_dolar = 20
+peso_mx= 10
+
+conversion = precio_dolar * peso_mx
+puts conversion
