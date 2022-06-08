fd6d288c4dcad47bd13a5bce674cbc0bd8f0b517
Emrys-Bryan <bryan.ibarra3522@gmail.com>
2022-06-08 12:47:06 -0400

    Generador letras

---
+++ desafios_ciclos_ruby/gen.rb
@@ -0,0 +1,15 @@
+#Realizar calculos
+def gen (n)
+    letra='a'
+    texto=''
+    n.times do 
+        texto=texto +letra
+        letra = letra.next
+    end
+return texto
+end
+
+
+#Mostrar en pantalla 
+puts gen(4) #abcd
+puts gen(10) #abcdefghij
