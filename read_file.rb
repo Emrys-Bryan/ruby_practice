8ef03e430e1de03131188f4c966b93f68c8a69d0
Emrys-Bryan <bryan.ibarra3522@gmail.com>
2022-06-10 11:56:27 -0400

    leyendo archivos

--- read_file.rb
+++ read_file.rb
@@ -2,7 +2,7 @@
 puts data = open('data.data').read
 #leyendo informacion archivos y pasando un array
 print data2= open('data.data').read.chomp.split(',')
-#leyendo lineas de un archivo
+#leyendo lineas de un archivo   
 reglones = File.readlines("texto.txt")
 puts ""
 reglones.each do |r|
